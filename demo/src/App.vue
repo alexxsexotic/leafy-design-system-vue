7e6fa71ce3074b2e08e5f38e2822947c5328e425
Xuan Anh Nguyen <alexsexotic@alexmac.home>
2019-08-04 09:36:58 +0200

    Added demo

---
+++ demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>hi</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  font-size: 15px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
+
---
+++ demo/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import './../../lib/styles/style.scss'
+
+new Vue({
+  render: function (h) { return h(App) },
+}).$mount('#app')
