0dce9fc979a8bf2e61e3e9c26cc81735abfa7cb7
Xuan Anh Nguyen <alexsexotic@Xuan-MacBook-Air.local>
2019-08-05 08:42:51 +0200

    neco

--- components/Google.vue
+++ components/Google.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="btn btn_default inline" @click="$emit('clickEvent',1)">
+  <button class="btn btn_default" @click="$emit('clickEvent',1)">
     <i class="icon">
       <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
