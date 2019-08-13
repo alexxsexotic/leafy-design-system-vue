7bdadd40850b06139d76ad3272433e82a4151e9b
Xuan Anh Nguyen <alexsexotic@alexmac.home>
2019-08-13 23:31:53 +0200

    s

--- components/Google.vue
+++ components/Google.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link class="btn btn_default" :to="link">
+  <a class="btn btn_default" :href="link">
     <i class="icon">
       <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
@@ -18,7 +18,7 @@
       </svg>
     </i>
     <slot></slot>
-  </router-link>
+  </a>
 </template>
 
 <script>
