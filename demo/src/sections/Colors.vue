75c2dc37c686e3c8e29dab36785a5b524993c796
Xuan Anh Nguyen <alexnguyen98@yahoo.com>
2019-09-01 00:04:19 +0200

    $1

---
+++ demo/src/sections/Colors.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <section>
+      <p>Colors</p>
+      <main>
+        <div>
+          <div class="block green-1"></div>
+          <div class="block green-2"></div>
+          <div class="block green-3"></div>
+        </div>
+        <div>
+          <div class="block blue-1"></div>
+          <div class="block blue-2"></div>
+        </div>
+        <div>
+          <div class="block bg-white"></div>
+          <div class="block border-white"></div>
+          <div class="block bg"></div>
+          <div class="block white"></div>
+        </div>
+      </main>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.block {
+  border: 2px solid rgba(0, 0, 0, 0.158);
+  display: block;
+  content: "";
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+}
+
+</style>
---
+++ demo/src/sections/Layouts.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <section>
+        <p>Layouts</p>
+        <div>
+          <h5>Client - Default</h5>
+          <div class="layout-client-default flex-col">
+            <div class="box nav"></div>
+            <div class="box main"></div>
+            <div class="box footer"></div>
+          </div>
+        </div>
+        <div>
+          <h5>Admin - Default</h5>
+          <div class="layout-admin-default flex-col">
+            <nav class="box"></nav>
+            <div class="inline">
+              <div class="box nav"></div>
+              <div class="box nav-2"></div>
+              <div class="box main"></div>
+            </div>
+          </div>
+        </div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.box {
+  content: "";
+  display: block;
+  background: #d3dce6;
+}
+
+.layout-client-default {
+  height: 400px;
+  .nav {
+    flex: 1;
+  }
+  .main {
+    margin: 5px 0;
+    flex: 5;
+  }
+  .footer {
+    flex: 1;
+  }
+}
+
+.layout-admin-default {
+  height: 400px;
+  nav {
+    flex: 1;
+    margin-bottom: 5px;
+  }
+  .inline {
+    flex: 6;
+  }
+  .nav {
+    flex: 1;
+  }
+  .nav-2 {
+    flex: 1;
+    margin: 0 5px;
+  }
+  .main {
+    flex: 5;
+  }
+}
+
+
+</style>
+
